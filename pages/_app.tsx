86015955e0175f7730c7fd2a1140b42c10e53b67
Thomas Malt <thomas@malt.no>
2021-09-18 20:12:45 +0200

    refactored to use themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import React from 'react';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>SD60 - Money Streams</title>
+        <meta name="description" content="Visualize money spent - and learn next.js" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
-export default MyApp
+
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2196f3',
+    },
+    secondary: {
+      main: '#ef6c00',
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: 'Roboto Slab',
+    },
+    h2: {
+      fontFamily: 'Roboto Slab',
+    },
+    h3: {
+      fontFamily: 'Roboto Slab',
+    },
+    h4: {
+      fontFamily: 'Roboto Slab',
+    },
+    h5: {
+      fontFamily: 'Roboto Slab',
+    },
+    h6: {
+      fontFamily: 'Roboto',
+    },
+  },
+});
