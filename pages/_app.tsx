bcceda639e7ea9798803876acf537579994e2fa4
Thomas Malt <thomas@malt.no>
2021-09-24 08:42:09 +0200

    adding a new page to deploy locally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,12 +18,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     >
       <Head>
         <title>SD60 - Money Streams</title>
-        <meta name="description" content="Visualize money spent - and learn next.js" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta name="description" content="Visualize money spent - and learn next.js" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <meta name="apple-mobile-web-app-status-bar" content={theme.palette.primary.main} />
+        <meta name="apple-mobile-web-app-title" content="Budget" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
--- styles/theme.ts
+++ styles/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#2196f3',
+      main: '#2195f3',
     },
     secondary: {
       main: '#ef6c00',
