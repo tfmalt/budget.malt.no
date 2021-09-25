81986448570f396d8aa0ee56796563d73146158b
Thomas Malt <thomas@malt.no>
2021-09-25 12:07:53 +0000

    Updates

---
+++ _next/static/xLoZKilQIPMqXIyd72ek1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/704-eeadb2b8d34cc52d9d78.js","static/css/3774af56fb7174185aea.css","static/chunks/pages/index-228b002937ba2f7cb89b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
