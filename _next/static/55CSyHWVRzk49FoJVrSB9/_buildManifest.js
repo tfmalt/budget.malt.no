e88ed978cee5c388e665ad38f9f9100d2288e3f1
tfmalt <thomas@malt.no>
2022-07-03 23:27:43 +0000

    Deploying to gh-pages from @ tfmalt/budget.malt.no@ee30e4b941894b4323f8bc29db5234be3f106fb3 🚀

--- _next/static/55CSyHWVRzk49FoJVrSB9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/479-ad2a45b57fa1d9a6.js","static/chunks/pages/index-fb93d364a9073dd2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/OFg4MyYDXiMpi1PSElGpn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/479-ad2a45b57fa1d9a6.js","static/chunks/pages/index-f3596796144c2730.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
