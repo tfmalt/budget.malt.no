dd97f69a382b22bdbae497308b4ef5f02dacfdf9
tfmalt <thomas@malt.no>
2022-07-03 23:08:00 +0000

    Deploying to gh-pages from @ tfmalt/budget.malt.no@1f821aa9792b0e40498b82f95a826f6d3c2947c2 🚀

--- _next/static/bBNOh3j1Mi3UsGh2VXuT7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/280-7e56d77d46dcb18e.js","static/css/ef9d3561c03cd8a2.css","static/chunks/pages/index-744ad96b8b43f140.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
