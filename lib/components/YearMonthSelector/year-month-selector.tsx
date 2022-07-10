337d3a431d092333a4ee1430027141b5672a8257
Thomas Malt <thomas@malt.no>
2022-07-10 19:29:08 +0000

    implemented support for whole year

--- lib/components/YearMonthSelector/year-month-selector.tsx
+++ lib/components/YearMonthSelector/year-month-selector.tsx
@@ -1,5 +1,5 @@
-import { Grid, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
-import React from 'react';
+import { Grid, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent, Divider } from '@mui/material';
+import * as React from 'react';
 import type { YearMonthSelectorProps } from '../../Budget.types';
 
 export const YearMonthSelector: React.VFC<YearMonthSelectorProps> = ({ onYearChange, onMonthChange, years }) => {
@@ -27,6 +27,7 @@ export const YearMonthSelector: React.VFC<YearMonthSelectorProps> = ({ onYearCha
   };
 
   const handleMonthChange = (e: SelectChangeEvent) => {
+    console.log('month change:', e.target.value);
     setMonth(e.target.value);
     typeof onMonthChange === 'function' && onMonthChange(e.target.value);
   };
@@ -35,7 +36,7 @@ export const YearMonthSelector: React.VFC<YearMonthSelectorProps> = ({ onYearCha
   return (
     <Grid container paddingTop={2} paddingBottom={2} spacing={3} direction="row" justifyContent="flex-start">
       <Grid item xs={6} sm={3} md={2} sx={{ minWidth: '288px' }}>
-        <FormControl variant="standard" sx={{ width: '100%' }}>
+        <FormControl variant="filled" sx={{ width: '100%' }}>
           <InputLabel id="budget-year-label">Year</InputLabel>
           <Select labelId="budget-year-label" id="budget-year-select" value={year} onChange={handleYearChange}>
             {years.map((year) => (
@@ -47,9 +48,13 @@ export const YearMonthSelector: React.VFC<YearMonthSelectorProps> = ({ onYearCha
         </FormControl>
       </Grid>
       <Grid item xs={6} sm={3} md={2} sx={{ minWidth: '288px', maxWidth: '343px' }}>
-        <FormControl variant="standard" sx={{ width: '100%' }}>
+        <FormControl variant="filled" sx={{ width: '100%' }}>
           <InputLabel id="budget-month-label">Month</InputLabel>
           <Select labelId="budget-month-label" id="budget-month-select" value={month} onChange={handleMonthChange}>
+            <MenuItem key={'All Year'} value={99}>
+              All Year
+            </MenuItem>
+            <Divider />
             {months.map((m) => (
               <MenuItem key={m[0]} value={m[1]}>
                 {m[0]}
