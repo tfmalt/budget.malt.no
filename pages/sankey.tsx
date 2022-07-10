ab6ad7a9afed91e426b24fe1c713e189434da1c6
Thomas Malt <thomas@malt.no>
2022-07-10 19:58:24 +0000

    attempt to fix login on sankey standalone page

--- pages/sankey.tsx
+++ pages/sankey.tsx
@@ -1,8 +1,11 @@
-import { Box, Container } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Box, Button, Container } from '@mui/material';
 import * as React from 'react';
 import { BudgetSankey } from '../lib/components/BudgetSankey';
+import { theme } from '../styles/theme';
 
 const SankeyPage = () => {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
   const now = new Date();
   const [year, setYear] = React.useState(now.getFullYear().toString());
   const [month, setMonth] = React.useState(now.getMonth().toString());
@@ -16,17 +19,35 @@ const SankeyPage = () => {
     return () => clearInterval(interval);
   }, []);
 
+  const handleLogin = () => {
+    loginWithRedirect({ connection: 'google-oauth2', redirect_uri: `${process.env.NEXT_PUBLIC_REDIRECT_URL}/sankey` });
+  };
+
   return (
     <Container disableGutters>
       <Box padding={2}>
-        <BudgetSankey
-          month={month}
-          year={year}
-          width={`100vw`}
-          maxHeight={`calc(100vh - 100px)`}
-          height={`calc(100vh - 100px)`}
-          update={lastUpdate}
-        />
+        {isAuthenticated ? (
+          <BudgetSankey
+            month={month}
+            year={year}
+            width={`100vw`}
+            maxHeight={`calc(100vh - 100px)`}
+            height={`calc(100vh - 100px)`}
+            update={lastUpdate}
+          />
+        ) : (
+          <Button
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              background: `linear-gradient(90deg, ${theme.palette.primary.main} 20%, #4db6ac 100%)`,
+            }}
+            variant="contained"
+            size="large"
+            onClick={handleLogin}
+          >
+            Sign in with Google
+          </Button>
+        )}
       </Box>
     </Container>
   );
